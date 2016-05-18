b57b6ef7e166372269d956c4b27a76039ec68d39
Fuji Haruka <fizzyjohn316@gmail.com>
2016-05-19 00:29:40 +0900

    見た目がそれっぽくなってきた

--- site/component/puzzle.js
+++ site/component/puzzle.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Util from '../util/util'
+import CONSTS from '../util/consts'
 
 const Puzzle = React.createClass({
   propTypes: {},
@@ -11,14 +12,26 @@ const Puzzle = React.createClass({
         <div className='puzzle-board'>
           {keys.map(key =>
             <div className='piece' key={key}>
-              <img className='piece-img' src={`./img/piece${key}.png`} key={key} />
+              <img className='piece-img' src='./img/flower.jpg' style={this.getPosition(key)} key={key} />
             </div>
           )}
+          <div className='piece' >空</div>
         </div>
       </div>
     )
-  }
+  },
 
+  getPosition (key) {
+    let index = (key + 15) % 16
+    let top = Math.floor(index / 4)
+    let left = index % 4
+    let size = CONSTS.PIECE_SIZE
+    let style = {
+      top: `${-1 * top * size}px`,
+      left: `${-1 * left * size}px`
+    }
+    return style
+  }
 })
 
 export default Puzzle
---
+++ site/util/consts.js
@@ -0,0 +1,5 @@
+const CONSTS = {
+  PIECE_SIZE: 155
+}
+
+export default CONSTS
